d05f8c85ae664611a6649d9b91c9508e51ed430d
nikolam00 <nikola.micovic00@gmail.com>
2021-12-31 03:31:30 +0100

    StudentController

--- Projekat/Startup.cs
+++ Projekat/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace Projekat
 {
@@ -26,6 +28,10 @@ namespace Projekat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SahCS"));
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
